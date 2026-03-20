7ffed7e998740b5e5f2d2f5b72c6f857fbb77be3
Alice <alicetassoreis@gmail.com>
2026-03-19 21:38:31 -0300

    Add corrections

--- src/main/java/com/boardcamp/controllers/GameController.java
+++ src/main/java/com/boardcamp/controllers/GameController.java
@@ -2,8 +2,13 @@ package com.boardcamp.controllers;
 
 import com.boardcamp.services.GameService;
 import com.boardcamp.models.Game;
+import com.boardcamp.dtos.GameDTO;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import jakarta.validation.Valid;
 
 import java.util.List;
 
@@ -20,5 +25,11 @@ public class GameController {
     @GetMapping
     public List<Game> getGames() {
         return service.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<Game> createGame(@Valid @RequestBody GameDTO dto) {
+        Game game = service.create(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(game);
     }
 }
